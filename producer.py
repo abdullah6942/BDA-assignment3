45ff79609f0953c068a9fddf7e91616c45d96995
abdullah6942 <164238553+abdullah6942@users.noreply.github.com>
2024-04-22 19:08:40 +0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -8,18 +8,29 @@ def delivery_callback(err, msg):
         print(f"Message delivered to topic {msg.topic()} partition {msg.partition()}")
 
 def produce_data(file_path, topic):
-    producer = Producer({'bootstrap.servers': 'localhost:9092', 'queue.buffering.max.messages': 1000000})
+    producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
     with open(file_path, 'r') as file:
-        for line in file:
-            # Produce each line as a message to Kafka topic
-            producer.produce(topic, value=line.rstrip(), callback=delivery_callback)
+        while True:
+            line = file.readline()
+            if not line:
+                break  # End of file here
+
+            try:
+                data = json.loads(line.strip())  # loading data line by line to avoid buffer issues
+                # Produce each JSON item as a message to Kafka topic
+                producer.produce(topic, value=json.dumps(data), callback=delivery_callback)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+
+            # Check if the producer queue is full
+            if producer.flush(0.1) > 0:  # Flush every 0.1 seconds
+                print("Producer queue full, flushing messages")
 
     # Wait for all messages to be delivered
     producer.flush()
 
 if __name__ == "__main__":
-    file_path = r'C:\Users\abdul\OneDrive\Desktop\cleaned2.json'
-
+    file_path = r'C:\Users\abdul\OneDrive\Desktop\cleaned4.json'
     kafka_topic = 'topic1'
     produce_data(file_path, kafka_topic)
